90fb3d6137fbc867041778b42a02e87203762481
Abenet Erbelo <abeneterbelo@gmail.com>
2026-01-23 09:56:05 +0300

    10

--- server.js
+++ server.js
@@ -21,22 +21,22 @@ database
  .then(() => console.log("Database connected"))
  .catch((err) => console.log("Error: " + err));
 
-// app.use(
-//   cors({
-//     origin: "*", 
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-    ],
-    credentials: true,
+    origin: "*", 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
   })
 );
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//     ],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   })
+// );
 
 app.use(express.json());
 app.use("/api/admin", adminRoutes);
